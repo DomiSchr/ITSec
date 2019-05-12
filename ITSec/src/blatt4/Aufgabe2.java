55c4a46ba56eb2a552576940c2160188ba5e1d07
Domi <schroederdomi@gmail.com>
2019-05-12 16:09:05 +0200

    Aufgabe 2 weiter, aber noch keine der Aufgaben

--- ITSec/src/blatt4/Aufgabe2.java
+++ ITSec/src/blatt4/Aufgabe2.java
@@ -1,21 +1,19 @@
 package blatt4;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Aufgabe2 {
 	public static void main(String[] args) {
 
-		BufferedReader reader;
-		String s;
+		String s = "";
 
 		try {
-			reader = new BufferedReader(new FileReader("text.txt"));
 
-			s = reader.readLine();
-			System.out.println(s);
+//			Zuerst die großen Buchstaben, danach Ä, Ö, Ü, die kleinen Buchstaben, ä, ö, ü, dann 0-9
+
+			s = new String(Files.readAllBytes(Paths.get("text.txt")), "UTF-8");
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -24,5 +22,41 @@ public class Aufgabe2 {
 
 		// a)
 
+		char c;
+		int[] anzahl = new int[67];
+
+		for (int i = 0; i < s.length(); i++) {
+			c = s.charAt(i);
+			anzahl[c % 67]++;
+		}
+
+		double[] warsch = new double[67];
+
+		// Ausgabe:
+		int i = 0;
+		while ((char) ('A' + i) != '[') {
+			System.out.println((char) ('A' + i) + ": " + anzahl[('A' + i) % 67]);
+			i++;
+		}
+
+		System.out.println("Ä: " + anzahl['Ä' % 67]);
+		System.out.println("Ö: " + anzahl['Ö' % 67]);
+		System.out.println("Ü: " + anzahl['Ü' % 67]);
+
+		i = 0;
+		while ((char) ('a' + i) != '{') {
+			System.out.println((char) ('a' + i) + ": " + anzahl[('a' + i) % 67]);
+			i++;
+		}
+
+		System.out.println("ä: " + anzahl['ä' % 67]);
+		System.out.println("ö: " + anzahl['ö' % 67]);
+		System.out.println("ü: " + anzahl['ü' % 67]);
+
+		i = 0;
+		while ((char) ('0' + i) != ':') {
+			System.out.println((char) ('0' + i) + ": " + anzahl[('0' + i) % 67]);
+			i++;
+		}
 	}
 }
