6f440ec9f34464ed483a2248e6fb42ab48cef998
Domi <schroederdomi@gmail.com>
2019-05-12 18:35:15 +0200

    Mit Aufgabe a)

--- ITSec/src/blatt4/Aufgabe2.java
+++ ITSec/src/blatt4/Aufgabe2.java
@@ -29,34 +29,63 @@ public class Aufgabe2 {
 			c = s.charAt(i);
 			anzahl[c % 67]++;
 		}
+		
+		int anzahlzeichen = s.length();
+		
 
-		double[] warsch = new double[67];
+//		 Anzahl:
+//		int i = 0;
+//		while ((char) ('A' + i) != '[') {
+//			System.out.println((char) ('A' + i) + ": " + anzahl[('A' + i) % 67]);
+//			i++;
+//		}
+//
+//		System.out.println("Ä: " + anzahl['Ä' % 67]);
+//		System.out.println("Ö: " + anzahl['Ö' % 67]);
+//		System.out.println("Ü: " + anzahl['Ü' % 67]);
+//
+//		i = 0;
+//		while ((char) ('a' + i) != '{') {
+//			System.out.println((char) ('a' + i) + ": " + anzahl[('a' + i) % 67]);
+//			i++;
+//		}
+//
+//		System.out.println("ä: " + anzahl['ä' % 67]);
+//		System.out.println("ö: " + anzahl['ö' % 67]);
+//		System.out.println("ü: " + anzahl['ü' % 67]);
+//
+//		i = 0;
+//		while ((char) ('0' + i) != ':') {
+//			System.out.println((char) ('0' + i) + ": " + anzahl[('0' + i) % 67]);
+//			i++;
+//		}
 
-		// Ausgabe: 
+//		 a) Informationsgehalt:
 		int i = 0;
 		while ((char) ('A' + i) != '[') {
-			System.out.println((char) ('A' + i) + ": " + anzahl[('A' + i) % 67]);
+			System.out.println((char) ('A' + i) + ": " + (double)(-(Math.log10((double)anzahl[('A' + i) % 67]/anzahlzeichen)/Math.log10(2.0))));
 			i++;
 		}
 
-		System.out.println("Ä: " + anzahl['Ä' % 67]);
-		System.out.println("Ö: " + anzahl['Ö' % 67]);
-		System.out.println("Ü: " + anzahl['Ü' % 67]);
+		System.out.println("Ä: " + (double)(-(Math.log10((double)anzahl[('Ä' + i) % 67]/anzahlzeichen)/Math.log10(2))));
+		System.out.println("Ö: " + (double)(-(Math.log10((double)anzahl[('Ö' + i) % 67]/anzahlzeichen)/Math.log10(2))));
+		System.out.println("Ü: " + (double)(-(Math.log10((double)anzahl[('Ü' + i) % 67]/anzahlzeichen)/Math.log10(2))));
 
 		i = 0;
 		while ((char) ('a' + i) != '{') {
-			System.out.println((char) ('a' + i) + ": " + anzahl[('a' + i) % 67]);
+			System.out.println((char) ('a' + i) + ": " + (double)(-(Math.log10((double)anzahl[('a' + i) % 67]/anzahlzeichen)/Math.log10(2))));
 			i++;
 		}
 
-		System.out.println("ä: " + anzahl['ä' % 67]);
-		System.out.println("ö: " + anzahl['ö' % 67]);
-		System.out.println("ü: " + anzahl['ü' % 67]);
+		System.out.println("ä: " + (double)(-(Math.log10((double)anzahl[('ä' + i) % 67]/anzahlzeichen)/Math.log10(2))));
+		System.out.println("ö: " + (double)(-(Math.log10((double)anzahl[('ö' + i) % 67]/anzahlzeichen)/Math.log10(2))));
+		System.out.println("ü: " + (double)(-(Math.log10((double)anzahl[('ü' + i) % 67]/anzahlzeichen)/Math.log10(2))));
 
 		i = 0;
 		while ((char) ('0' + i) != ':') {
-			System.out.println((char) ('0' + i) + ": " + anzahl[('0' + i) % 67]);
+			System.out.println((char) ('0' + i) + ": " + (double)(-(Math.log10((double)anzahl[('0' + i) % 67]/anzahlzeichen)/Math.log10(2))));
 			i++;
 		}
+
 	}
 }
