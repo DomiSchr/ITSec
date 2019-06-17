cefa116d5079afa267126126a34b71462552b9bf
DomiSchr <domischroeder@gmx.de>
2019-06-17 13:18:35 +0200

    9/1 fertig

---
+++ src/blatt9/ErwEuklid.java
@@ -0,0 +1,26 @@
+package blatt9;
+
+import java.util.Arrays;
+
+public class ErwEuklid {
+
+	static int[] ErwEuklid(int a, int b) {
+		if (b == 0) {
+			int[] arr = { a, 1, 0 };
+			return arr;
+		} else {
+			int[] arr = ErwEuklid(b, a % b);
+			int at = arr[1];
+			int bt = arr[2];
+			arr[1] = bt;
+			arr[2] = at - (bt * (a / b));
+
+			return arr;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(ErwEuklid()));
+	}
+}
